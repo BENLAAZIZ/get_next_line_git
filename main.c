5d6406bd9c406ac8cf3d7835472c81bbabd88586
BEN LAAZIZ HAMZA <benlaaziz.hamza@gmail.com>
2023-12-17 11:03:38 +0100

    update

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: hben-laz <hben-laz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/14 17:55:14 by hben-laz          #+#    #+#             */
-/*   Updated: 2023/12/14 17:55:17 by hben-laz         ###   ########.fr       */
+/*   Updated: 2023/12/17 11:02:38 by hben-laz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,43 +15,33 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-#define BUFFER_SIZE 1024 // Adjust the buffer size as needed
+int main()
+{
+	static char buffer[100];
 
-int main() {
-    int fd = open("example.txt", O_RDONLY);
-    if (fd == -1) {
-        perror("Error opening file");
-        return 1;
-    }
+	int fd = open("test.txt", O_RDONLY);
+	 
+	if (fd == -1)
+	{
+		perror("Error opening file");
+		exit(EXIT_FAILURE);
+	}
 
-    char *line = (char *)malloc(BUFFER_SIZE + 1);
-    if (line == NULL) {
-        perror("Error allocating memory");
-        close(fd);
-        return 1;
-    }
+	size_t a = read(fd, buffer, sizeof(buffer));
+	
+	
 
-    ssize_t bytesRead;
-    int i = 0;
-
-    while ((bytesRead = read(fd, &line[i], 1)) > 0) {
-        if (line[i] == '\n') {
-            line[i + 1] = '\0'; // Terminate the line with null character
-            printf("Line: %s\n", line);
-            i = 0; // Reset index for the next line
-        } else {
-            i++;
-        }
-    }
-
-    if (bytesRead == -1) {
-        perror("Error reading file");
-        free(line);
-        close(fd);
-        return 1;
-    }
-
-    free(line);
-    close(fd);
-    return 0;
+    if (a == -1) 
+	{
+		perror("Error reading file");
+		close(fd);
+		exit(EXIT_FAILURE);
+	}
+	buffer[a] = '\0';
+	printf("%d\n", fd);
+    printf("Read %zd bytes from the file:\n%s\n", a, buffer);
+	write(0, "tsxsjhx", fd);
+	
+	close(fd);
+	return 0;
 }
---
+++ pp.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int f(int x)
+{
+	static int sum = 0;
+	return (sum +=x); 
+}
+int main ()
+{
+	printf("%d", f(5));
+	printf("%d", f(5));
+	printf("%d", f(5));
+	printf("%d", f(5));
+	printf("%d", f(5));
+	printf("%d", f(5));
+
+	printf("%d", f(5));
+	printf("%d", f(5));
+	printf("%d", f(5));
+	printf("%d", f(5));
+	printf("%d", f(5));
+}
