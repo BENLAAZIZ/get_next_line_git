fe0ca9a29cd9b7129fff3b0da865b21bbafac635
BEN LAAZIZ HAMZA <benlaaziz.hamza@gmail.com>
2024-01-06 16:28:28 +0100

    update

--- get_next_line.c
+++ get_next_line.c
@@ -5,40 +5,110 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: hben-laz <hben-laz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/12/12 15:56:37 by hben-laz          #+#    #+#             */
-/*   Updated: 2024/01/03 19:36:49 by hben-laz         ###   ########.fr       */
+/*   Created: 2024/01/06 04:13:50 by hben-laz          #+#    #+#             */
+/*   Updated: 2024/01/06 16:28:03 by hben-laz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
 #include "get_next_line.h"
-#include <unistd.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <fcntl.h>
+
+int	chek_new_line(char *buf, int *n)
+{
+	int	i;
+
+	i = 0;
+	n = &i;
+	if (buf == NULL)
+		return 0;
+	while (buf[i])
+	{
+		if (buf[i] == '\n')
+			return (1);
+		i++;
+	}
+	n = &i;
+	return (i);
+}
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	l;
+
+	l = 0;
+	while (*s != '\0')
+	{
+		l++;
+		s++;
+	}
+	return (l);
+}
+
+char	*ft_strdup(const char *s1)
+{
+	size_t	size;
+	char	*tab;
+
+	size = ft_strlen(s1);
+	tab = (char *)malloc(sizeof(char) * (size + 1));
+	if (tab == NULL)
+		return (NULL);
+	if (size > 0)
+		ft_memcpy(tab, s1, size);
+	tab[size] = '\0';
+	return (tab);
+}
 
 
-// #ifndef BUFFER_SIZE 5
-// #define BUFFER_SIZE 5
-// #endif
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	size_t	i;
+	size_t	j;
+	char	*tab;
+
+	i = 0;
+	if (s1 == NULL && s2 == NULL)
+		return (NULL);
+	if (s1 == NULL)
+		return (ft_strdup(s2));
+	if (s2 == NULL)
+		return (ft_strdup(s1));
+	tab = (char *)malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char)); 
+	if (tab == NULL)
+		return (NULL);
+	while (i < ft_strlen(s1))
+	{
+		tab[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (j < ft_strlen(s2))
+		tab[i++] = s2[j++];
+	tab[i] = '\0';
+	return (tab);
+}
 
 
-// int ft_strlen(char *buffer)
-// {
-// 	int i = 0;
-// 	while (buffer[i])
-// 		i++;
-// 		return (i);
-// }
 char	*get_next_line(int fd)
 {
-	char static	*buf;
-	int char_read;
-	char static *line;
-	buf = malloc(5 * sizeof(char) + 1);
-	char_read = read(fd, buf, 5);
-	buf[6] = '\0';
-	if (fd <= 0)
-		return (0);
-	return (buf);
+    char *buf;
+    char *line = NULL;
+    int n = 0;
+    
+    if (fd < 0 || !(buf = (char *)malloc(BUFFER_SIZE + 1)))
+        return NULL;
+
+    while (read(fd, buf, BUFFER_SIZE) > 0)
+    {
+        buf[BUFFER_SIZE] = '\0';
+        if (chek_new_line(buf, &n))
+            break;
+        
+        char *buf_save = ft_strdup(buf);
+        line = ft_strjoin(line, buf_save);
+        free(buf_save);
+    }
+
+    free(buf);
+
+    return line;
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,12 +6,19 @@
 /*   By: hben-laz <hben-laz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/12 15:57:19 by hben-laz          #+#    #+#             */
-/*   Updated: 2023/12/14 13:05:26 by hben-laz         ###   ########.fr       */
+/*   Updated: 2024/01/04 18:30:02 by hben-laz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GIT_NEXT_LINE_H
 # define GIT_NEXT_LINE_H
+# ifndef BUFFER_SIZE
+# define BUFFER_SIZE 5
+# endif
+# include <unistd.h>
+# include <string.h>
+# include <stdlib.h>
+# include <fcntl.h>
 
 char	*get_next_line(int fd);
 
