2692809a7f7195e587984e3287493159f5332695
BEN LAAZIZ HAMZA <benlaaziz.hamza@gmail.com>
2024-01-13 19:07:05 +0100

    update

--- main.c
+++ main.c
@@ -5,26 +5,42 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: hben-laz <hben-laz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/12/17 13:40:16 by hben-laz          #+#    #+#             */
-/*   Updated: 2024/01/11 17:29:36 by hben-laz         ###   ########.fr       */
+/*   Created: 2024/01/12 15:43:50 by hben-laz          #+#    #+#             */
+/*   Updated: 2024/01/13 19:01:51 by hben-laz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line_bonus.h" 
+#include "get_next_line.h"
 #include <stdio.h>
 
-
-int main ()
+int	main()
 {
+	//read from multipl files
+	
+	// int fd1 = open("file1.txt" , O_CREAT | O_RDONLY);
+	// int fd2 = open("file2.txt",O_CREAT | O_RDONLY);
+	// int fd3 = open("file3.txt",O_CREAT | O_RDONLY);
+	// int i = 0;
+	// char *s;
+	// while((s = get_next_line(3 + i % 3)))
+	// {
+	// 	puts(s);
+	// 	free(s);
+	// 	i++;
+	// }
 
-	int		fd;
-	fd = open("fil.txt", O_RDONLY | O_CREAT);
-	char *c;
-	while((c = get_next_line(fd)))
+	//write and read
+	char *str = "hug";
+	int fd = open("fil.txt", O_CREAT | O_RDWR);
+	while (1)
 	{
-		printf("%s", c);
-		free(c);
-		c = NULL;
+		str = get_next_line(0);
+		write(fd, str, strlen(str));
+	
 	}
+	{
+		/* code */
+	}
+	
 	return (0);
 }
